c237d6ae91d1bb2c7e1a3f228b2bf390967bd907
Jeff Chung <chungchi300@hotmail.com>
2015-12-28 18:42:04 +0800

    c

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -1,7 +1,9 @@
 package hello;
 
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +19,8 @@ public class HelloController {
 
         return "yolo";
     }
-
+    @RequestMapping("/hello/{name}")
+    public @ResponseBody String hello(@PathVariable("name") String name){
+        return "Hello" + name;
+    }
 }
